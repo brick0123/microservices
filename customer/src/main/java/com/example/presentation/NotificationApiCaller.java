457f26e2d13e5f9edf76e6130c4ba162b8bf77d2
brick0123 <tlfanfvk@gmail.com>
2022-06-12 13:11:12 +0900

    feat: add k8s profile

--- customer/src/main/java/com/example/presentation/NotificationApiCaller.java
+++ customer/src/main/java/com/example/presentation/NotificationApiCaller.java
@@ -3,7 +3,7 @@ package com.example.presentation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "notification")
+@FeignClient(name = "notification", url = "${clients.notification.url}")
 public interface NotificationApiCaller {
 
     @PostMapping("api/v1/notification")
--- fraud/src/main/java/com/example/FraudApplication.java
+++ fraud/src/main/java/com/example/FraudApplication.java
@@ -3,8 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@PropertySources({
+    @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 @EnableEurekaClient
 @EnableJpaAuditing
 @SpringBootApplication
